1f9f6e3a7bcdfa638241bc4c74149d7133911e7c
Swathi-quabyt <96561374+Swathi-quabyt@users.noreply.github.com>
2024-05-08 09:00:01 +0530

    Create taskService.jsx

---
+++ services/taskService.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3001';
+
+export const getBoardData = async () => {
+  const response = await axios.get(`${BASE_URL}/board`);
+  return response.data;
+};
